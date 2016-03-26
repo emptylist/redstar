36a79552d26770ed5ac1e706df73fcfe60d92e95
emptylist <crooks1379@gmail.com>
2016-03-26 12:03:00 -0500

    Weapon firing and simple projectile system added

--- game.ts
+++ game.ts
@@ -1,17 +1,31 @@
 window.onload = main;
 var DEBUG = true;
 
+var SCREEN = (function (){
+  var bkgrnd: any = document.getElementById("background");
+  return {
+    HEIGHT: bkgrnd.height,
+    WIDTH: bkgrnd.width
+  }
+})();
+
 function main() {
   var background = document.getElementById("background");
   var foreground = document.getElementById("foreground");
   var hud = document.getElementById("hud");
   var game = new Game(window, background, foreground, hud);
   var ts = performance.now();
+  if (DEBUG) {
+    var debugFPS = document.getElementById("fps");
+  }
 
   function update(time) {
     var dt = time - ts;
     ts = time;
     game.update(dt / 1000);
+    if (DEBUG) {
+      debugFPS.innerHTML = "FPS: " + Math.floor(1.0/(dt/1000));
+    }
     window.requestAnimationFrame(update);
   }
 
@@ -25,6 +39,8 @@ class Game {
   controlSchema: ControlSchema;
   playerController: Controller;
   player: Entity;
+  clear: boolean;
+  playerProjectiles: ProjectileManager;
 
   constructor(w, background, foreground, hud) {
     this.hud = hud;
@@ -32,25 +48,49 @@ class Game {
     this.foreground = foreground.getContext("2d");
     this.controlSchema = new DefaultControlSchema();
     this.playerController = new PlayerController(this.controlSchema);
-    this.player = new Entity(new Vector(100, 100)
+    this.playerProjectiles = new ProjectileManager();
+    var basicWeapon = new Weapon("Bit Shooter", 
+                                 "Shoots bitmaps", 
+                                  3, 
+                                  bitShooterProjs, 
+                                  this.playerProjectiles);
+    this.player = new Player(new Vector(300, 320)
       , spriteMaker("player")
       , new PlayerTimestepper()
-      , this.playerController);
+      , this.playerController
+      , basicWeapon);
+    this.clear = true;
 
-    w.onkeydown = (function (controller) { return (function (e) {
+    w.onkeydown = (function (controller, game) { 
+      var keytraps = [32];
+      return (function (e) {
+      console.log(e.which);
+      if (e.which === 16) {
+        game.clear = false;
+      }
       controller.processInput(e);
-    })})(this.playerController);
+      if (keytraps.some(function (key) { return key == e.which })) {
+        e.preventDefault()
+      }
+    })})(this.playerController, this);
 
-    w.onkeyup = (function (controller) { return (function (e) {
+    w.onkeyup = (function (controller, game) { return (function (e) {
+      if (e.which === 16) {
+        game.clear = true;
+      }
       controller.processInput(e);
-    })})(this.playerController);
+    })})(this.playerController, this);
       
   }
 
   update(dt: number): void {
     this.player.update(dt);
-    this.foreground.clearRect(0, 0, 600, 640);
+    this.playerProjectiles.update(dt);
+    if (this.clear) {
+      this.foreground.clearRect(0, 0, 600, 640);
+    }
     this.player.render(this.foreground);
+    this.playerProjectiles.render(this.foreground);
   }
 }
 
@@ -130,12 +170,12 @@ interface Timestepper {
 }
 
 interface Controller {
-  update(actor: Entity): void;
+  update: (actor: Entity) => void;
 }
 
 interface Command {
   cmdname: string;
-  execute(actor: Entity): void;
+  execute: (actor: Controllable) => void;
 }
 
 class MoveState {
@@ -152,19 +192,19 @@ class MoveState {
   }
 }
 
+interface Controllable {
+  controller: Controller;
+}
+  
 class Entity implements Renderable {
   position: Vector;
   sprite: Sprite;
-  stepper: Timestepper;
-  controller: Controller;
-  moveState: MoveState;
+  timestepper: Timestepper;
 
-  constructor(p: Vector, s: Sprite, stp: Timestepper, c: Controller) {
+  constructor(p: Vector, s: Sprite, stp: Timestepper) {
     this.position = p;
     this.sprite = s;
-    this.stepper = stp;
-    this.controller = c;
-    this.moveState = new MoveState();
+    this.timestepper = stp;
   }
 
   render(ctx: CanvasRenderingContext2D): void {
@@ -172,8 +212,88 @@ class Entity implements Renderable {
   }
 
   update(dt: number): void {
+    this.timestepper.step(dt, this)
+  }
+}
+
+class Projectile extends Entity {
+  power: number;
+
+  constructor(p: Vector, s: Sprite, stp: Timestepper, power: number) {
+    super(p, s, stp);
+    this.power = power;
+  }
+}
+
+class ProjectileManager  implements Renderable {
+  private projectiles: Array<Projectile>;
+
+  constructor() {
+    this.projectiles = new Array<Projectile>();
+  }
+
+  update(dt: number): void {
+    for (var i = 0; i < this.projectiles.length; i++) {
+      var p = this.projectiles[i];
+      p.update(dt);
+      if (p.position.x > SCREEN.WIDTH ||
+          (p.position.x + p.sprite.width < 0) ||
+          p.position.y > SCREEN.HEIGHT ||
+          (p.position.y + p.sprite.height < 0)) {
+        this.projectiles.splice(i,i);
+      }
+    }
+  }
+
+  render(ctx: CanvasRenderingContext2D): void {
+    for (var i = 0; i < this.projectiles.length; i++) {
+      this.projectiles[i].render(ctx);
+    }
+  }
+
+  addProjectile(proj: Projectile): void {
+    this.projectiles.push(proj);
+  }
+}
+
+class Weapon {
+  name: string;
+  desc: string;
+  rechargeTime: number;
+  rechargeTimer: number;
+  fire: (position: Vector) => void;
+
+  constructor(name: string, 
+              desc: string, 
+              rechargeTime: number, 
+              fire: (p: Vector) => Projectile,
+              projectileManager: ProjectileManager)
+              {
+    this.name = name;
+    this.desc = desc;
+    this.rechargeTime = rechargeTime;
+    this.fire = function (pos: Vector) {
+      projectileManager.addProjectile(fire(pos));
+    };
+    this.rechargeTimer = 0;
+  }
+}
+
+class Player extends Entity implements Controllable {
+  controller: Controller;
+  weapon: Weapon;
+  moveState: MoveState;
+
+  constructor(p: Vector, s: Sprite, stp: Timestepper, c: Controller, w: Weapon) {
+    super(p, s, stp);
+    this.controller = c;
+    this.weapon = w;
+    this.moveState = new MoveState();
+  }
+
+  update(dt: number): void {
     this.controller.update(this);
-    this.stepper.step(dt, this)
+    this.timestepper.step(dt, this);
   }
 }
 
@@ -181,15 +301,31 @@ class PlayerTimestepper implements Timestepper {
   private SPEED: number;
 
   constructor() {
-    this.SPEED = 40;
+    this.SPEED = 100;
   }
-  step(dt: number, actor: Entity): void {
+  step(dt: number, actor: Player): void {
     var velocity = new Vector(0, 0);
     if (actor.moveState.moveLeft) { velocity.x -= 1.0; }
     if (actor.moveState.moveRight) { velocity.x += 1.0; }
     if (actor.moveState.moveUp) { velocity.y -= 1.0; }
     if (actor.moveState.moveDown) { velocity.y += 1.0; }
     actor.position.translate(velocity.mul(dt * this.SPEED));
+    actor.weapon.rechargeTimer = Math.max(actor.weapon.rechargeTimer - dt, 0);
+  }
+}
+
+class SimpleBulletTimestepper implements Timestepper {
+  private SPEED: number;
+  private VELOCITY: Vector;
+
+  constructor() {
+    this.SPEED = 120;
+    this.VELOCITY = new Vector(0, -1);
+    this.VELOCITY.scale(this.SPEED);
+  }
+
+  step(dt: number, actor: Entity): void {
+    actor.position.translate(this.VELOCITY.mul(dt));
   }
 }
 
@@ -247,7 +383,7 @@ class MoveLeft implements Command {
     this.active = active;
   }
 
-  execute(actor: Entity) {
+  execute(actor: Player) {
     actor.moveState[this.cmdname] = this.active;
   }
 }
@@ -261,7 +397,7 @@ class MoveRight implements Command {
     this.active = active;
   }
 
-  execute(actor: Entity) {
+  execute(actor: Player) {
     actor.moveState[this.cmdname] = this.active;
   }
 }
@@ -275,7 +411,7 @@ class MoveUp implements Command {
     this.active = active;
   }
 
-  execute(actor: Entity) {
+  execute(actor: Player) {
     actor.moveState[this.cmdname] = this.active;
   }
 }
@@ -289,8 +425,23 @@ class MoveDown implements Command {
     this.active = active;
   }
   
-  execute(actor: Entity) {
+  execute(actor: Player) {
     actor.moveState[this.cmdname] = this.active;
+  }
+}
+
+class FireWeapon implements Command {
+  cmdname: string;
+
+  constructor () {
+    this.cmdname = "fireWeapon";
+  }
+
+  execute(actor: Player) {
+    if (actor.weapon.rechargeTimer === 0) {
+      actor.weapon.fire(actor.position);
+      actor.weapon.rechargeTimer = actor.weapon.rechargeTime
+    }
   }
 }
 
@@ -301,7 +452,7 @@ class NullCommand implements Command {
     this.cmdname = "NullCommand";
   }
 
-  execute(actor: Entity) {}
+  execute(actor: Player) {}
 }
 
 interface ControlSchema {
@@ -310,6 +461,7 @@ interface ControlSchema {
 
 class DefaultControlSchema implements ControlSchema {
   private schema: Object;
+  private nullCommand: NullCommand;
   constructor() {
     this.schema = {
       "keydown": {
@@ -317,21 +469,40 @@ class DefaultControlSchema implements ControlSchema {
       , 68: new MoveRight(true)
       , 83: new MoveDown(true)
       , 87: new MoveUp(true)
+      , 32: new FireWeapon()
       },
       "keyup": {
         65: new MoveLeft(false)
       , 68: new MoveRight(false)
       , 83: new MoveDown(false)
       , 87: new MoveUp(false)
+      , 32: new FireWeapon()
       }
     };
+    this.nullCommand = new NullCommand();
   }
 
   processInput(e: KeyboardEvent): Command {
     var cmd = this.schema[e.type][e.which];
     if (!cmd) {
-      cmd = new NullCommand();
+      cmd = this.nullCommand;
     }
     return cmd;
   }
 }
+
+var bitShooterProjs = (function() {
+  var canvas = document.createElement("canvas");
+  canvas.height = 4;
+  canvas.width = 4;
+  var ctx = canvas.getContext("2d");
+  ctx.fillStyle = "steelblue";
+  ctx.fillRect(0, 0, 4, 4);
+  var sprite = spriteMaker("bits");
+  var stp = new SimpleBulletTimestepper();
+  return (function (p: Vector) {
+    var projP = new Vector(p.x, p.y);
+    return new Projectile(projP, sprite, stp, 1.0);
+  })
+})();
+
